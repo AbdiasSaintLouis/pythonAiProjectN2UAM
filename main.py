866fc52bba17aa7429eebf32ca9d3f4992b10693
Abdias Saint Louis <abiwriter.sp@gmail.com>
2023-06-11 22:28:48 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+import openpyxl
+from PyQt5.QtWidgets import QApplication, QFileDialog, QTableView
+from ui import MainWindow
+from data import load_data
+from pandasModel import pandasModel
+
+
+if __name__ == '__main__':
+    # criar a aplicação
+    app = QApplication(sys.argv)
+
+    # criar a janela da interface gráfica do usuário
+    window = MainWindow()
+    window.show()
+
+    # carregar dados de um arquivo
+    file_name, _ = QFileDialog.getOpenFileName(window, "Carregar Dados", "",
+                                               "Arquivos CSV (*.csv);;Arquivos Excel (*.xlsx);;Arquivos JSON (*.json);;Todos os Arquivos (*)")
+    if file_name:
+        file_type = file_name.split('.')[-1]  # obter a extensão do arquivo
+        data = load_data(file_name, file_type)
+
+        # exibir os dados em uma tabela
+        data_table = QTableView()
+        data_table.setModel(pandasModel(data))
+        window.layout.addWidget(data_table)
+
+    # carregar dados de um banco de dados
+    else:
+        host = 'localhost'
+        user = 'myuser'
+        password = 'mypassword'
+        db = 'mydatabase'
+        table = 'mytable'
+
+        data = load_data(host=host, user=user, password=password, db=db, table=table)
+
+        # exibir os dados em uma tabela
+        data_table = QTableView()
+        data_table.setModel(pandasModel(data))
+        window.layout.addWidget(data_table)
+
+    # executar a aplicação
+    sys.exit(app.exec_())
